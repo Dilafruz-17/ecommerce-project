a4f6d79bc4186aa65dafa588d6cc94b88472f2a3
Dilafruz Urunboyeva <dilafruzurinboyeva70@gmail.com>
2025-12-10 22:55:36 +0500

    product rasmlariga js added

--- js/product.js
+++ js/product.js
@@ -1,1 +1,8 @@
- 
+const smallImages = document.querySelectorAll('.small');
+const bigImage = document.querySelector('.big');
+
+smallImages.forEach(img => {
+    img.addEventListener('click', () => {
+        bigImage.src = img.src;
+    });
+});
